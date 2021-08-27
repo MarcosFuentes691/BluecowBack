7c35ebb8429e251d619c0a57ba14b2d7361931dd
Marcos Fuentes <marcosfuentes691@gmail.com>
2021-08-27 12:17:37 -0300

    Documentation changes

--- src/main/java/com/bluecow/consts/SwaggerConstant.java
+++ src/main/java/com/bluecow/consts/SwaggerConstant.java
@@ -6,16 +6,16 @@ public class SwaggerConstant {
     public static final String AUTHORIZATION_DESCRIPTION = "Full api access";
     public static final String AUTHORIZATION_SCOPE = "Unlimited";
     public static final String SECURE_PATH = "/*/.*";
-    public static final String API_TITLE = "Disney API";
-    public static final String API_DESCRIPTION = "Api with information about characters,movies and series of Disney<br>" +
-            "In order to execute requests you first need to register at */auth/register* and then to login at */auth/login* to get a token<br>" +
+    public static final String API_TITLE = "Bluecow API";
+    public static final String API_DESCRIPTION = "Api of <br>" +
+            "In order to execute requests you first need to register and login (the easiest way to do this is to login via the frontend) to get a token<br>" +
             ",which you need to execute requests, to use it you need the enter the following 'Bearer *Your token*' in the authorize field";
     public static final String API_VERSION = "1.0";
     public static final String LICENSE = "";
     public static final String LICENSE_URL = "";
     public static final String CONTACT_NAME = "Marcos";
     public static final String CONTACT_URL = "";
-    public static final String CONTACT_EMAIL = "marcos691@gmail.com";
+    public static final String CONTACT_EMAIL = "marcosfuentes691@gmail.com";
     public static final String TERM_OF_SERVICE = "";
 
 }
